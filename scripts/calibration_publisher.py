77d7180c28f203a08e24923d05ef0d1639cc5051
Ravi Prakash Joshi <ravi2008joshi@gmail.com>
2018-05-01 22:57:52 +0900

    Update calibration_publisher.py

--- scripts/calibration_publisher.py
+++ scripts/calibration_publisher.py
@@ -28,10 +28,10 @@ def main():
     # create tf listener and broadcaster
     tf_broadcaster = tf2_ros.StaticTransformBroadcaster()
 
-    all_transformations = []
+    transformations = []
     for calib in calibration:
         # load calibration file
-        param_file = os.path.join(data_dir, ('baxter_%s_calibration.yaml' % calib))
+        param_file = os.path.join(data_dir, 'baxter_%s_calibration.yaml' % calib)
         rospy.loginfo('Reading file:\n%s\n' % param_file)
 
         with open(param_file, 'r') as f:
@@ -43,25 +43,25 @@ def main():
         trans = params['trans']
         parent = params['parent']
 
-        static_transform = TransformStamped()
+        transformation = TransformStamped()
 
-        static_transform.header.stamp = rospy.Time.now()
-        static_transform.header.frame_id = parent
-        static_transform.child_frame_id = child
+        transformation.header.stamp = rospy.Time.now()
+        transformation.header.frame_id = parent
+        transformation.child_frame_id = child
 
-        static_transform.transform.translation.x = trans[0]
-        static_transform.transform.translation.y = trans[1]
-        static_transform.transform.translation.z = trans[2]
-        static_transform.transform.rotation.x = rot[0]
-        static_transform.transform.rotation.y = rot[1]
-        static_transform.transform.rotation.z = rot[2]
-        static_transform.transform.rotation.w = rot[3]
+        transformation.transform.translation.x = trans[0]
+        transformation.transform.translation.y = trans[1]
+        transformation.transform.translation.z = trans[2]
+        transformation.transform.rotation.x = rot[0]
+        transformation.transform.rotation.y = rot[1]
+        transformation.transform.rotation.z = rot[2]
+        transformation.transform.rotation.w = rot[3]
 
-        all_transformations.append(static_transform)
+        transformations.append(transformation)
 
     # src: https://answers.ros.org/question/261815
-    # Publish static transformation
-    tf_broadcaster.sendTransform(all_transformations)
+    # publish static transformation
+    tf_broadcaster.sendTransform(transformations)
     rospy.spin()
 
 
