725eda20034f5c2fe4fdee90753baa3127610cc9
Ravi Prakash Joshi <ravi2008joshi@gmail.com>
2018-05-01 22:59:03 +0900

    Update change_grippers.py

--- scripts/change_grippers.py
+++ scripts/change_grippers.py
@@ -18,18 +18,16 @@ def main():
     # initialize ros node
     rospy.init_node('change_grippers', anonymous=True)
 
-    rospy.loginfo('Please wait for sometime. Change grippers process should take %s seconds approximately.' % (
-        sleep_duration * 2))
+    rospy.loginfo('Please wait for sometime. Change grippers process should take %s seconds approximately.' % (sleep_duration * 2))
 
     left_cmd = 'rosrun baxter_examples send_urdf_fragment.py -f $(rospack find baxter_description)/urdf/left_end_effector.urdf.xacro -l left_hand -j left_gripper_base'
     right_cmd = 'rosrun baxter_examples send_urdf_fragment.py -f $(rospack find baxter_description)/urdf/right_end_effector.urdf.xacro -l right_hand -j right_gripper_base'
 
-    all_cmd = [left_cmd, right_cmd]
+    cmds = [left_cmd, right_cmd]
 
     # source: https://stackoverflow.com/a/4791612
-    for cmd in all_cmd:
-        p = subprocess.Popen(
-            cmd, shell=True, stderr=subprocess.PIPE, preexec_fn=os.setsid)
+    for cmd in cmds:
+        p = subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE, preexec_fn=os.setsid)
         # let the parameters reach to baxter controller
         rospy.sleep(sleep_duration)
         # Send the signal to all the process groups
