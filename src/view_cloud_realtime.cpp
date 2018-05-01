cb5a6ca5afb53da35d975cf4523675629e9bc1b7
Ravi Prakash Joshi <ravi2008joshi@gmail.com>
2018-05-01 22:46:33 +0900

    Update view_cloud_realtime.cpp

--- src/view_cloud_realtime.cpp
+++ src/view_cloud_realtime.cpp
@@ -16,27 +16,6 @@
 #include <pcl/visualization/pcl_visualizer.h>
 
 
-/*
-// old code. not using. todo: clean
-//std::string pointCloudTopic = "/kinect_anywhere/point_cloud/points2";
-//std::string pointCloudTopic = "/kinect1/sd/points";
-
-inline void PointCloudXYZRGBAtoXYZRGB(pcl::PointCloud<pcl::PointXYZRGBA>& in,
-    pcl::PointCloud<pcl::PointXYZRGB>& out) {
-  out.width = in.width;
-  out.height = in.height;
-  out.points.resize(in.points.size());
-  for (size_t i = 0; i < in.points.size(); i++) {
-    out.points[i].x = in.points[i].x;
-    out.points[i].y = in.points[i].y;
-    out.points[i].z = in.points[i].z;
-    out.points[i].r = in.points[i].r;
-    out.points[i].g = in.points[i].g;
-    out.points[i].b = in.points[i].b;
-  }
-}
-*/
-
 pcl::visualization::PCLVisualizer viewer("Realtime Cloud Viewer");
 
 void callback(
@@ -44,39 +23,6 @@ void callback(
   pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(
       new pcl::PointCloud<pcl::PointXYZRGB>);
   utility::getPointCloudFromMsg(msg, *cloud);
-
-  /*
-  std::vector<int> indices;
-  pcl::PCLPointCloud2 pcl_pc2;
-  pcl::PointCloud < pcl::PointXYZRGBA > temp_cloud;
-  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(
-      new pcl::PointCloud<pcl::PointXYZRGB>);
-  */
-
-  /*
-   * It was found that even though point cloud ros message field says that
-   * libfreenect2 point cloud is 'rgb', it is 'rgba'. Hence the code below
-   * assumes that incoming point cloud is 'rgba' and it converts it to 'rgb'
-   * to further use
-   */
-  /*
-  pcl_conversions::toPCL(*msg, pcl_pc2);
-  pcl::fromPCLPointCloud2(pcl_pc2, temp_cloud);
-  PointCloudXYZRGBAtoXYZRGB(temp_cloud, *cloud);
-  */
-
-  /*
-   * point cloud received from libfreenect2 shows that it is dense point cloud
-   * which means it shouldn't contain any 'nan' but 'nan' was found. Hence in
-   * order to remove 'nan', we first need to make it non-dense. we should
-   * make it dense, once 'nan' are removed.
-   */
-
-   /*
-  cloud->is_dense = false;
-  pcl::removeNaNFromPointCloud(*cloud, *cloud, indices);
-  cloud->is_dense = true;
-  */
 
   if (!viewer.updatePointCloud(cloud, "cloud"))
     viewer.addPointCloud(cloud, "cloud");
